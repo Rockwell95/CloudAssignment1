19163e7e952548a5e2b36a9fbd4b49ff77a745cf
Rockwell95 <dominick.mancini@uoit.net>
2017-01-26 18:36:30 -0500

    Uh

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express();
 
-app.use(express.static('Assign1/www'));
+app.use(express.static('www'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
